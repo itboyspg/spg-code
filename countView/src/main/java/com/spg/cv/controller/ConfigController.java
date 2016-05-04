89ca00d2dff38b1a4c0bfabad463af28108eca97
spg <itboy_spg@163.com>
2016-05-04 22:31:10 +0800

    增加埋点配置项管理

--- countView/src/main/java/com/spg/cv/controller/ConfigController.java
+++ countView/src/main/java/com/spg/cv/controller/ConfigController.java
@@ -32,10 +32,52 @@ public class ConfigController extends BaseController
     @Resource
     ConfigService configService;
 
+    /**
+     * @description: 增加一个配置
+     * @author: Wind-spg
+     * @param request
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value = "addConfig", method = RequestMethod.POST, produces =
     { "application/json;charset=UTF-8" })
     public String addConfig(HttpServletRequest request)
+    {
+        String dataType = request.getParameter("dataType");
+        String englishName = request.getParameter("englishName");
+        String chineseDesc = request.getParameter("chineseDesc");
+        PVBean pvBean = new PVBean(englishName, chineseDesc);
+
+        DataType newDataType = null;
+        if (StringUtils.isNotEmpty(dataType))
+        {
+            try
+            {
+                newDataType = DataType.getEnumByCode(Integer.parseInt(dataType));
+            } catch (Exception e)
+            {
+                LOGGER.error(e);
+                return buildFailedResultInfo(-1, e.getMessage());
+            }
+        }
+        if (StringUtils.isEmpty(englishName) || StringUtils.isEmpty(chineseDesc))
+        {
+            return buildFailedResultInfo(-1, "页面英文名或中文描述为空！");
+        }
+        Long result = configService.addConfig(newDataType, pvBean);
+        return buildSuccessResultInfo(result);
+    }
+
+    /**
+     * @description: 删除一个埋点配置项
+     * @author: Wind-spg
+     * @param request
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "deleteConfig", method = RequestMethod.POST, produces =
+    { "application/json;charset=UTF-8" })
+    public String deleteConfig(HttpServletRequest request)
     {
         String dataType = request.getParameter("dataType");
         String englishName = request.getParameter("englishName");
