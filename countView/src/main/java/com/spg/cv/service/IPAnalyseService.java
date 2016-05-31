e1560be15ec7a8aaae628b9cbd917cfbb0bb3128
spg <itboy_spg@163.com>
2016-05-31 23:13:37 +0800

    添加IP分析功能

---
+++ countView/src/main/java/com/spg/cv/service/IPAnalyseService.java
@@ -0,0 +1,17 @@
+
+package com.spg.cv.service;
+
+
+/**
+ * 项目名称：countView
+ *
+ * @description:
+ * @author Wind-spg
+ * @create_time：2016年5月31日 下午11:11:26
+ * @version V1.0.0
+ */
+public interface IPAnalyseService
+{
+
+}
+
---
+++ countView/src/main/java/com/spg/cv/service/impl/IPAnalyseServiceImpl.java
@@ -0,0 +1,23 @@
+
+package com.spg.cv.service.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
+import com.spg.cv.service.IPAnalyseService;
+
+/**
+ * 项目名称：countView
+ *
+ * @description:
+ * @author Wind-spg
+ * @create_time：2016年5月31日 下午11:12:43
+ * @version V1.0.0
+ */
+@Service(value = "iPAnalyseService")
+public class IPAnalyseServiceImpl implements IPAnalyseService
+{
+    private static final Log LOGGER = LogFactory.getLog(IPAnalyseServiceImpl.class);
+}
+
